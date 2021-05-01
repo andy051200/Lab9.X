64c49110f9d739599f98fe77e723c596b8ede67b
andy051200 <bon19451@uvg.edu.gt>
2021-05-01 12:02:05 -0600

    seguimiento de lab

---
+++ lab9.X/build/default/production/main_lab9.p1.d
@@ -0,0 +1,2 @@
+build/default/production/main_lab9.p1:  \
+main_lab9.c 
--- lab9.X/main_lab9.c
+++ lab9.X/main_lab9.c
@@ -4,11 +4,11 @@ Microcontrolador: PIC16F887
 Autor: Andy Bonilla
 Compilador: pic-as (v2.30), MPLABX v5.45
     
-Programa: Laboratorio 8, medidor de dos potenciometros
+Programa: Laboratorio 9, mover servo motores con modulo PWM
 Hardware: PIC16F887
     
-Creado: 20 de abril de 2021    
-Descripcion: valores pot1 en leds de PORTA, valores pot2 en displays PortC
+Creado: 27 de abril de 2021    
+Descripcion: motor servo en P1A y P1B
 ------------------------------------------------------------------------------*/
 // CONFIG1
 #pragma config FOSC = INTRC_NOCLKOUT// se declara oscilador interno
@@ -30,33 +30,17 @@ Descripcion: valores pot1 en leds de PORTA, valores pot2 en displays PortC
 ------------------------librerias a implementar ------------------------------
 -----------------------------------------------------------------------------*/
 #include <xc.h>
-#include <stdint.h>
-#define  _XTAL_FREQ 4000000  //se define el delay con FreqOsc 4Mhz
+#define  _XTAL_FREQ 8000000  //se define el delay con FreqOsc 4Mhz
 
 /*-----------------------------------------------------------------------------
 ------------------------directivas del compilador------------------------------
 -----------------------------------------------------------------------------*/
-#define _tmr0_value 237
+//#define _tmr0_value 237
 
 /*-----------------------------------------------------------------------------
 -------------------------------- variables -----------------------------------
 -----------------------------------------------------------------------------*/
-//se declara una cadena equivalente a la tabla de valores binarios de displays
-unsigned char nums_displays[] = 
-{
-    0b00111111, //0
-    0b00000110, //1
-    0b01011011, //2
-    0b01001111, //3
-    0b01100110, //4
-    0b01101101, //5
-    0b01111101, //6
-    0b00000111, //7
-    0b01111111, //8
-    0b01101111  //9
-};
-unsigned int a,b,c ; //a para centenas, b para decenas, c para unidades
-unsigned int contador = 0; // varible para incrementar
+
 
 /*-----------------------------------------------------------------------------
 -------------------------prototipos de funciones-------------------------------
@@ -66,28 +50,23 @@ void setup(void);  //funcion para configuracion de registros
 
 void __interrupt() isr(void) //funcion de interrupciones
 {
-    //si en caso hay interrupcion por overflow del timer 0
-    if (T0IF==1)
-    { 
-        a = ((contador/100)%10) ; // valor del contador lo divide en centenas
-        b = ((contador/10)%10) ;  // valor del contador lo divide en decenas
-        c = (contador%10) ;        // valor del contador en unidades
-        T0IF=0; //se apaga la bandera de interrupcion
-    }
     
-    //si en caso hay interrupcion por ADC, ADCON0bits.CHS==11
+    //si en caso hay interrupcion por ADC
     if (PIR1bits.ADIF==1) 
     {
-        if (ADCON0bits.CHS==11)
+        if (ADCON0bits.CHS==0)
+        {
+            //el valor del servo 1   
+            CCPR1L = ADRESH;
+            PIR1bits.ADIF=0;
+        }
+        else 
         //{
-            PORTA=ADRESH; //el valor del adresh se pasa a PortA con Leds
-        //}
-        else
-        //{
-            contador=ADRESH; //el valor del   
-            //PORTA=ADRESH;
-        //}       
-        PIR1bits.ADIF=0; // se aaga bandera de interrupcion de ADC
+            //servo2=ADRESH;//el valor del servo 2
+            PORTDbits.RD0=1;
+            
+            PIR1bits.ADIF=0;
+        //}     
     }
 }
 
@@ -98,42 +77,26 @@ void __interrupt() isr(void) //funcion de interrupciones
 void main(void)  //funcion principal sin retorno
 {  
     setup(); //en el main se manda a llamar a las configuraciones
-    __delay_ms(20);
-    ADCON0bits.GO = 1; //se activa medición inicial
-    
+    //ADCON0bits.GO=1; //medicion inicial
     //---------------------loop principal del programa ------------------------
     while(1) //se hace loop infinito mientras sea 1
     {
-            
-        //este bloque es para hacer el cambio en los tranaistores PortE   
-        PORTC = nums_displays[a];
-        PORTEbits.RE0 = 1;
-        __delay_ms(10);
-        PORTEbits.RE0 = 0;
-        
-        PORTC = nums_displays[b];
-        PORTEbits.RE1 = 1;
-        __delay_ms(10);
-        PORTEbits.RE1 = 0;
-          
-        PORTC = nums_displays[c];
-        PORTEbits.RE2 = 1;
-        __delay_ms(10);
-        PORTEbits.RE2 = 0;
-              
-        if (ADCON0bits.GO==0)
-        {
-            if (ADCON0bits.CHS==11)
+        if (ADCON0bits.GO ==0)
+        {   
+            if (ADCON0bits.CHS==0)
             {
-                ADCON0bits.CHS=13;
+                ADCON0bits.CHS=1;
+                __delay_us(100);//delay 100us para cambio de canal
+                ADCON0bits.GO = 1;
             }
             else
-            //{
-                ADCON0bits.CHS=11; 
-                __delay_us(100);//delay 50us para cambio de canal
+            {
+                ADCON0bits.CHS=0; 
+                __delay_us(100);//delay 100us para cambio de canal
                 ADCON0bits.GO = 1;
-            //}
+            }
         }
+        
     }
 }
 
@@ -143,50 +106,72 @@ void main(void)  //funcion principal sin retorno
 void setup(void) //FUNCION PARA CONFIGURACION DE ENTRADAS Y SALIDAS
 {
     //CONFIGURACION DE ENTRADAS/SALIDAS DIGITALES
-    ANSEL = 0X00;  
-    ANSELH = 0b00110000; //pines de entrada analogica 11 y 13
+    ANSEL = 0b00000011; //se congigura AN0 y AN1 como entrada analógica  
+    ANSELH = 0; //pines de entrada analogica 11 y 13
+       
+    //CONFIGURACION DE ENTRADAS/SALIDAS ANLAGÓGICAS
+    TRISA = 0xFF;   // se define canal de salida
+    TRISC= 0;       // se define PortC como salida
+    TRISD= 0;       // se define PortC como salida
+    PORTC=0;        // se define POrtC como salida
+    PORTD=0;        // se define POrtC como salida
     
-    //CONFIGURACION DE ENTRADAS Y SALIDAS EN PUERTOS
-    TRISA = 0X00;       // Todo el puerto A es salida
-    TRISB = 0b00110000;  // PortB4 y PortB5 son entradas
-    TRISC = 0X00;       // Todo el PortC como salida display 7seg
-    TRISE = 0x00;       // Todo el PortE como salida de transistores
-            
-    PORTA = 0X00;       // Todo el puerto A es salida
-    PORTB = 0b00110000; // PortB4 y PortB5 son entradas
-    PORTC = 0X00;       // Todo el puerto C es salida
-    PORTE = 0x00;       // Todo el puerto E es salida 
-           
     //CONFIGURACION DEL OSCILADOR
-    OSCCONbits.IRCF2=1; //4MHz 110
-    OSCCONbits.IRCF1=1; //4MHz 110
-    OSCCONbits.IRCF0=0; //4MHz 110
+    OSCCONbits.IRCF2=1; //8MHz 111
+    OSCCONbits.IRCF1=1; //8MHz 111
+    OSCCONbits.IRCF0=1; //8MHz 111
     OSCCONbits.SCS=1;   //configuracion de oscilador interno
-    
-    //CONFIGURACION DEL TIMER0
-    OPTION_REGbits.T0CS=0;
-    OPTION_REGbits.PSA=0; //SE HABILITA EL PREESCALER
-    OPTION_REGbits.PS2=1; //PREESCALER 1:256 111
-    OPTION_REGbits.PS1=1; //PREESCALER 1:256 111
-    OPTION_REGbits.PS0=1; //PREESCALER 1:256 111
-        
+            
     //CONFIGURACION DE ADC
-    ADCON1bits.ADFM = 0 ; // se justifica a la isquierda
-    ADCON1bits.VCFG0 = 0 ; // voltajes de referencia
-    ADCON1bits.VCFG1 = 0 ; // voltaje de referencia
+    ADCON1bits.ADFM = 0 ;        // se justifica a la iZquierda
+    ADCON1bits.VCFG0 = 0 ;      // voltajes de referencia 5V
+    ADCON1bits.VCFG1 = 0 ;      // voltaje de referencia gnd
     
-    ADCON0bits.ADCS = 0b01 ; // se usa division de 4us con F/8
-    ADCON0bits.CHS = 11; // seleccion de canal 11
-    __delay_us(50);         //delay de 50us para que cargue capacitor
-    ADCON0bits.ADON = 1 ; // se prende modulo ADC
+    ADCON0bits.ADCS = 0b10 ;    // se usa division de 4us con F/32
+    ADCON0bits.CHS = 0;         // seleccion de canal 11
+    ADCON0bits.ADON = 1 ;       // se prende modulo ADC
+    __delay_us(50);             //delay de 50us para que cargue capacitor
+    //ADCON0bits.ADON = 1 ;       // se prende modulo ADC
     
-    //CONFIGURACION DE INTERRUPCIONES
+    
+    //CONFIGURACION DEL modulo PWM
+    //CCP1 para servo 1
+    TRISCbits.TRISC2=1;        //RC2/CCP1 como entrada a motor se desconecta
+    TRISDbits.TRISD5=1;        //RC2/CCP1 como entrada a motor se desconecta
+    PR2 = 249;                  //configurando el periodo
+    
+    //CCP1 para servo 1
+    CCP1CONbits.P1M = 0b0;      // configuracion de una señales de salida
+    CCP1CONbits.CCP1M = 0b1100; //  se configura como modo PWM
+    CCPR1L = 0x0f ;         //ciclo de tabajo inicial de la onda cuadradad
+    CCP1CONbits.DC1B = 0;   //
+
+    //CCP2 para servo 2
+    CCP2CON = 0x0C;      // configuracion de una sola señal
+    //CCP2CONbits.DC2B1 =0;      // configuracion de una sola señal
+    //CCP2CONbits.CCP2M = 0b1100; //  se configura como modo PWM
+    CCPR2L = 100 ;         //ciclo de tabajo inicial de la onda cuadradad
+    //CCP2CONbits. = 0;   //
+  
+    
+   
+    //configruacion del timer2
+    PIR1bits.TMR2IF = 0;     //bajar bandera de interrupcion del timer2
+    T2CONbits.T2CKPS = 0b11;  //preescaler del timer2 a tope 1:16
+    T2CONbits.TMR2ON = 1;       //se prende el timer
+    
+    while(PIR1bits.TMR2IF==0);  //ciclo para que nunca se prenda bandera
+    PIR1bits.TMR2IF=0;      // se apaga bandera por si las moscas
+    TRISCbits.TRISC2 = 0;     //salida del pwm1
+    TRISDbits.TRISD5= 0;        // salida del pwm 2
+     //CONFIGURACION DE INTERRUPCIONES
     INTCONbits.GIE=1; //se habilitan las interrupciones globales
     INTCONbits.PEIE=1 ; //se prende interrupcion por perifericos
     INTCONbits.T0IE=1; //enable bit de int timer0
     INTCONbits.TMR0IF=0; //se apaga la bandera de int timer0
     INTCONbits.TMR0IE=1; // enable bit de IntOnCHangePortB
     INTCONbits.RBIF=0; // se apaga la bandera de IntOnChangeB  
+    
     //CONFIGURACION INTERRUPCION DEL ADC
     PIE1bits.ADIE = 1 ; //se prende interrupcion por ADC
     PIR1bits.ADIF = 0; // se baja bandera de conversion
